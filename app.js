c0d1c85091f11b443fb053c815d22a0e4f1a7063
Artem Riabykh <riabykhartem@yandex.ru>
2023-12-09 16:57:10 -0600

    VM initialising

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 const mongoose = require('mongoose');
